acf24c04e33cc0b040e14b87669da767991b12b3
test <caihua_0@163.com>
2017-11-17 10:52:31 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+//整个应用的根组件
+<template>
+<div class="container" id="app">
+  <transition name="fade">
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
+  </transition>
+</div>
+</template>
+
+<script>
+// import Login from './Loading.vue'
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+  // ,
+  // components: { Loading }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/UserInfo.vue
@@ -0,0 +1,143 @@
+//用户信息
+
+<template>
+<div class="info">
+  <mt-header fixed title="信息" class="tophead">
+<!--     <router-link to="/" slot="left">
+      <mt-button icon="back">back</mt-button>
+    </router-link> -->
+    <mt-button slot="right" @click="logout">注销</mt-button>
+  </mt-header>
+  <div class="head">
+
+  </div>
+  <div class="body">
+    <ul class="ullist">
+      <li class="lilist">
+        <img class="icon" src="./images/下载.png" @click="addday">
+        <span class="name">积分</span><span class="number">
+        <mt-badge size="small">30</mt-badge></span>
+      </li>
+      <li class="lilist">
+        <img class="icon" src="./images/下载.png" >
+        <span class="name">奖金</span>
+        <span class="number"><mt-badge size="small"type="error">30</mt-badge></span>
+      </li>
+      <li class="lilist">
+        <img class="icon" src="./images/下载.png" >
+        <span class="name">假期</span>
+        <span class="number"><mt-badge size="small" type="warning">30</mt-badge></span>
+      </li>
+    </ul>
+  </div>
+  <mt-loadmore :top-method="loadTop" :bottom-method="loadBottom" ref="loadmore">
+    <mt-cell  v-for="item in list" title="操作类型">{{ item }}</mt-cell>
+  </mt-loadmore>
+</div>
+
+</template>
+<script>
+import Loading from './Loading.vue'
+import { MessageBox } from  'mint-ui'
+export default {
+  name: 'UserInfo',
+  data(){
+  	return {
+      isLogouting: false,
+      list:['2016-01-01','2016-01-02','2016-01-03','2016-01-04']
+    }
+  },
+  components:{
+    Loading
+  },
+  computed: {
+  	levelClass(){
+  		var level = this.level;
+  		if (1 <= level && level <= 7) {
+        	return 1;
+      	} else if (8 <= level && level <= 16) {
+        	return 2;
+      	} else if (16 <= level && level <= 31) {
+        	return 3;
+      	} else if (32 <= level && level <= 63) {
+        	return 4;
+      	} else if (64 <= level && level <= 127) {
+          	return 5;
+      	} else if (128 <= level && level <= 254) {
+        	return 6;
+      	} else {
+        	return 6;
+      	}
+  	}
+  },
+  methods: {
+    //注销
+    logout(){
+      //删除cookie并跳到登录页
+      this.isLogouting = true;
+      this.delCookie('session');
+      //演示
+      setTimeout(()=>{
+        this.$router.push('/login/');
+        this.isLogouting = false;
+      },3000)
+    },
+    loadTop(){
+      this.$refs.loadmore.onTopLoaded();
+      
+    },
+    loadBottom(){
+      // this.allLoaded = true;
+      this.$refs.loadmore.onBottomLoaded();
+    },
+    addday(){
+      MessageBox.confirm('确定执行此操作?').then(action => {
+        console.log(2);
+      },()=>{
+        console.log(3);
+      }
+      );
+    }
+  }
+}
+</script>
+
+<style scoped>
+.tophead{
+  background: #31c27c;/*绿色*/
+}
+.head{
+  width: 100%;
+  height: 80px;
+  margin-top: 40px;
+  background: #31c27c;/*绿色*/
+  
+}
+.ullist{
+  list-style: none;
+  display: flex;
+  justify-content: space-between;
+  margin: 0;
+  padding: 20px;
+}
+.lilist{
+
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+}
+.lilist>.name{
+  font-size: 14px;
+}
+.lilist>.number{
+  margin-left: 10px;
+}
+.lilist>.icon{
+    display: inline-block;
+    width: 30px;
+    height: 30px;
+    margin-right: 10px;
+}
+
+</style>
